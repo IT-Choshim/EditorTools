3ba48a40b5df4449bcf1d78107c81efe2ee61b96
lizhi <king_liz@163.com>
2019-01-03 00:08:36 +0800

    修改window目录

---
+++ Assets/Code/Window/AWindow.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class AWindow {
+
+    public AWindow(string name)
+    {
+        subwindowDictionary = new Dictionary<string, SubWindow>();
+    }
+
+    public AWindow(Transform transform)
+    {
+        subwindowDictionary = new Dictionary<string, SubWindow>();
+    }
+
+    public Dictionary<string, SubWindow> subwindowDictionary { get; protected set; }
+
+
+    /// <summary>
+    /// 添加subwindow
+    /// </summary>
+    /// <param name="name">Name.</param>
+    /// <param name="sub">Sub.</param>
+    public void AddSubwindow(string name,SubWindow sub)
+    {
+        if (!subwindowDictionary.ContainsKey(name))
+        {
+            this.subwindowDictionary[name] = sub;
+        }
+    }
+
+
+    /// <summary>
+    /// 打开subwindow的open()
+    /// </summary>
+    /// <param name="name">Name.</param>
+    /// <param name="o">O.</param>
+    public void OpenSubwindow(string name,WindowData windowData = null)
+    {
+        SubWindow sub;
+        if(subwindowDictionary.TryGetValue(name,out sub))
+        {
+            sub.Open(windowData);
+        }
+    }
+
+
+    #region 子窗口
+
+    /// <summary>
+    /// 初始化子窗口
+    /// </summary>
+    public virtual void Init()
+    {
+
+    }
+
+
+    /// <summary>
+    /// 打开自窗口
+    /// </summary>
+    /// <param name="data">Data.</param>
+    public virtual void Open(WindowData data = null)
+    {
+
+    }
+
+
+    /// <summary>
+    /// 关闭子窗口
+    /// </summary>
+    public virtual void Close()
+    {
+
+    }
+
+    #endregion
+
+}
---
+++ Assets/Code/Window/SubWindow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubWindow:AWindow {
+
+    public SubWindow(string name) : base(name)
+    {
+        subWindowDictionary = new Dictionary<string, object>();
+    }
+
+    public SubWindow(Transform transform) : base(transform)
+    {
+        subwindowDictionary = new Dictionary<string, SubWindow>();
+    }
+
+
+    public Dictionary<string, object> subWindowDictionary { get; private set; }
+
+
+    /// <summary>
+    /// add subWindowDictionary
+    /// </summary>
+    /// <param name="name">Name.</param>
+    /// <param name="o">O.</param>
+    public void AddData(string name,object o)
+    {
+        if (!subWindowDictionary.ContainsKey(name))
+        {
+            subWindowDictionary.Add(name,o);
+        }
+        else
+        {
+            Debug.Log("老铁 你所提供的name，subWindowDictionary中已存在，请检查");
+        }
+    }
+
+
+    /// <summary>
+    /// remove掉subwindowdictionary
+    /// </summary>
+    /// <param name="name">Name.</param>
+    /// <param name="o">O.</param>
+    public void RemoveData(string name, object o)
+    {
+        if (subWindowDictionary.ContainsKey(name))
+        {
+            subWindowDictionary.Remove(name);
+        }
+    }
+
+
+    /// <summary>
+    /// Removes all.
+    /// </summary>
+    public void RemoveAll()
+    {
+        subWindowDictionary.Clear();
+    }
+
+
+    /// <summary>
+    /// Close this instance.
+    /// </summary>
+    public override void Close()
+    {
+        base.Close();
+    }
+
+}
